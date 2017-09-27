3f84d7775946c81d5f1f0b513a5fec2ea5c1c831
afeiship <1290657123@qq.com>
2017-09-27 10:54:53 +0800

    issue: add remove all method

--- dist/next-set.js
+++ dist/next-set.js
@@ -2,49 +2,55 @@
 
   var global = global || this || self || window;
   var nx = global.nx || require('next-js-core2');
-  var _ ;
+  var _;
   _ = nx.remove || require('next-remove');
   _ = nx.unique || require('next-unique');
 
   var NxSet = nx.declare('nx.Set', {
-    properties:{
+    properties: {
       size: {
-        get: function(){
+        get: function () {
           return this.elements.length;
         }
       }
     },
-    methods:{
-      init: function(inArray){
+    methods: {
+      init: function (inArray) {
         this.elements = nx.unique(inArray) || [];
       },
-      add: function(inValue){
-        if( this.has(inValue) ){
+      add: function (inValue) {
+        if (this.has(inValue)) {
           this.remove(inValue);
         }
         this.elements.push(inValue);
       },
-      remove: function(inValue){
-        return nx.remove(this.elements, [ inValue ] );
+      addAll: function (inItems) {
+        var result = this.elements.concat(inItems);
+        this.elements = nx.unique(result);
       },
-      clear: function(){
+      remove: function (inValue) {
+        return nx.remove(this.elements, [inValue]);
+      },
+      removeAll: function (inItems) {
+        return nx.remove(this.elements, inItems);
+      },
+      clear: function () {
         this.elements = [];
       },
-      has: function(inValue){
+      has: function (inValue) {
         for (var index = 0; index < this.elements.length; index++) {
           var element = this.elements[index];
-          if( inValue === element ){
+          if (inValue === element) {
             return true;
           }
         }
         return false;
       },
-      toArray: function(){
+      toArray: function () {
         return this.elements;
       }
     }
   });
-
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = NxSet;
--- test/test.js
+++ test/test.js
@@ -11,6 +11,14 @@ describe('next-treemap', function () {
     assert.deepEqual(s1.toArray(), [1,2]);
   });
 
+  it('next-set has', function () {
+    var s1 = new nx.Set([1,2,1]);
+
+    assert.equal(s1.has(1), true);
+    assert.equal(s1.has(2), true);
+    assert.equal(s1.has(3), false);
+  });
+
 
   it('next-set add', function () {
     var s1 = new nx.Set([1,2,1]);
@@ -19,6 +27,33 @@ describe('next-treemap', function () {
 
     assert.equal(s1.size, 4);
     assert.deepEqual(s1.toArray(), [1,2,5,6]);
+  });
+
+  it('next-set addAll', function () {
+    var s1 = new nx.Set([1,2,3]);
+    s1.addAll([2,3,4]);
+
+    assert.equal(s1.size, 4);
+    assert.deepEqual(s1.toArray(), [1,2,3,4]);
+  });
+
+
+
+  it('next-set remove', function () {
+    var s1 = new nx.Set([1,2,3,2]);
+    s1.remove(2);
+
+    assert.equal(s1.size, 2);
+    assert.deepEqual(s1.toArray(), [1,3]);
+  });
+
+
+  it('next-set removeAll', function () {
+    var s1 = new nx.Set([1,2,3,2]);
+
+    s1.removeAll([1,2]);
+    assert.equal(s1.size, 1);
+    assert.deepEqual(s1.toArray(), [ 3 ]);
   });
 
 
